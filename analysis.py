2c60ad8d35e7b4a106c00bbe825bb1ce0f17c0f3
Michael Benz <michael.benzlg@yahoo.de>
2021-07-29 20:07:46 +0200

    Optimized the occurence of terms check

--- analysis.py
+++ analysis.py
@@ -2,10 +2,14 @@ import lzma
 import os
 import random
 import time
+import re
+import string
 from argparse import ArgumentParser
 from flair.data import Sentence
 from flair.models import TextClassifier
 from tqdm import tqdm
+
+from gensim.parsing.preprocessing import remove_stopwords
 
 def collectArgs():
     parser = ArgumentParser()
@@ -16,7 +20,66 @@ def collectArgs():
     parser.add_argument('-n', '--num_samples', dest='num_samples', help='Amount of files to sample', default="10", type=int)
     parser.add_argument('-e', '--exchange_word', dest='exchange_word', help='Switch the word with an alternative before classification')
     parser.add_argument('-m', '--mode', dest='mode', help='Specifies the mode of operation')
-    return parser.parse_args()    
+    return parser.parse_args()
+
+def get_first_occurence_case_insensitive(line, args):
+    line_low = line.lower()
+    word_low = args.word.lower()
+    index_match = line_low.find(word_low)
+    while index_match != -1:
+        # Checks if the substring is used as a standalone word and not as a substring in another word. Boundary check included.
+        if (index_match == 0 or not line_low[index_match - 1].isalpha()) and (len(line_low) == index_match + len(word_low) or not line_low[index_match + len(word_low)].isalpha()):
+            return index_match
+        else:
+            index_match = line_low.find(word_low, index_match + 1)
+    return -1
+
+def run_get_cooccurences(args):
+    data_file_list = os.listdir(args.data_directory)
+    random.seed(args.seed)
+    sampled_ids = random.sample(range(0, len(data_file_list)), args.num_samples)
+    total_count = 0
+    occurence_map = {}
+    for i in tqdm(range(len(sampled_ids))):
+        data_file = data_file_list[sampled_ids[i]]
+        with lzma.open(os.path.join(args.data_directory, data_file), mode='rt', encoding='utf8') as file:
+            for line in file:
+                if get_first_occurence_case_insensitive(line, args) != -1:
+                    words_without_stopwords = remove_stopwords(line)
+                    for word in words_without_stopwords.split(' '):
+                        cleaned = word.lower().translate(str.maketrans('', '', string.punctuation))
+                        if cleaned in occurence_map:
+                            occurence_map[cleaned] += 1
+                        else:
+                            occurence_map[cleaned] = 1
+    sorted_occurence_map = sorted(occurence_map.items(), key=lambda x: x[1], reverse=True)
+    print(sorted_occurence_map[:200])
+
+
+def run_get_sentences_count_with_word(args):
+    data_file_list = os.listdir(args.data_directory)
+    random.seed(args.seed)
+    sampled_ids = random.sample(range(0, len(data_file_list)), args.num_samples)
+    total_count = 0
+    for i in tqdm(range(len(sampled_ids))):
+        data_file = data_file_list[sampled_ids[i]]
+        with lzma.open(os.path.join(args.data_directory, data_file), mode='rt', encoding='utf8') as file:
+            for line in file:
+                if get_first_occurence_case_insensitive(line, args) != -1:
+                    total_count += 1
+    print("Total Count of " + args.word + ": " + str(total_count))
+
+def run_get_sentences_with_word(args):
+    data_file_list = os.listdir(args.data_directory)
+    random.seed(args.seed)
+    sampled_ids = random.sample(range(0, len(data_file_list)), args.num_samples)
+    for i in tqdm(range(len(sampled_ids))):
+        data_file = data_file_list[sampled_ids[i]]
+        with lzma.open(os.path.join(args.data_directory, data_file), mode='rt', encoding='utf8') as file:
+            for line in file:
+                is_present, word_cased, exchange_word_cased = find_case_insensitive(line, args.word, args.exchange_word)
+                if is_present:
+                    print(line)
 
 # Performs sentiment analysis on a single sentence.
 def run_single_classification(args):
@@ -39,11 +102,11 @@ def run_difference_classification(args):
         data_file = data_file_list[sampled_ids[i]]
         with lzma.open(os.path.join(args.data_directory, data_file), mode='rt', encoding='utf8') as file:
             for line in file:
-                is_present, word_cased, exchange_word_cased = find_case_insensitive(line, args.word, args.exchange_word)
-                if is_present:
-                        line_original = crunch_passage(line, word_cased, 100)
+                word_index = get_first_occurence_case_insensitive(line, args)
+                if word_index != -1:
+                        line_original = crunch_passage(line.lower(), word_index, args.word.lower(), 100)
                         if args.exchange_word:
-                            line_replaced = line_original.replace(word_cased, exchange_word_cased)
+                            line_replaced = line.replace(args.word.lower(), args.exchange_word.lower())
                         sentence_original = Sentence(line_original)
                         sentence_replaced = Sentence(line_replaced)
                         classifier.predict(sentence_original)
@@ -72,11 +135,11 @@ def run_classification(args):
         data_file = data_file_list[sampled_ids[i]]
         with lzma.open(os.path.join(args.data_directory, data_file), mode='rt', encoding='utf8') as file:
             for line in file:
-                is_present, word_cased, exchange_word_cased = find_case_insensitive(line, args.word, args.exchange_word)
-                if is_present:
-                        line = crunch_passage(line, word_cased, 100)
+                word_index = get_first_occurence_case_insensitive(line, args)
+                if word_index != -1:
+                        line = crunch_passage(line.lower(), word_index, args.word.lower(), 100)
                         if args.exchange_word:
-                            line = line.replace(word_cased, exchange_word_cased)
+                            line = line.replace(args.word.lower(), args.exchange_word.lower())
                         sentence = Sentence(line)
                         classifier.predict(sentence)
                         if str(sentence.labels[0]).split(' ')[0] == 'POSITIVE':
@@ -107,18 +170,19 @@ def find_case_insensitive(line, word, exchange_word):
     if exchange_word is None:
         exchange_word = ''
     if word in line:
-        return True, word, exchange_word
+        return True, isolate(word), isolate(exchange_word)
     if word.upper() in line:
-        return True, word.upper(), exchange_word.upper()
+        return True, isolate(word.upper()), isolate(exchange_word.upper()) 
     if word.lower() in line:
-        return True, word.lower(), exchange_word.lower()
+        return True, isolate(word.lower()), isolate(exchange_word.lower())
     return False, word, exchange_word
 
+def isolate(word):
+    return ' ' + word + ' '
 
 # Reduces a line to the size of max_len by expanding equally left and right of the keyword
-def crunch_passage(line, word, max_len):
+def crunch_passage(line, index, word, max_len):
     
-    index = line.find(word)
     if index == -1:
         return ''
 
@@ -150,5 +214,11 @@ def run():
         run_single_classification(args)
     if args.mode == 'diff':
         run_difference_classification(args)
+    if args.mode == 'list':
+        run_get_sentences_with_word(args)
+    if args.mode == 'count':
+        run_get_sentences_count_with_word(args)
+    if args.mode == 'co':
+        run_get_cooccurences(args)
 
 run()
